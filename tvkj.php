d465e60457554beeda797962452968ed37831d20
CAT <99686044+cloudcat214@users.noreply.github.com>
2024-07-20 11:22:55 +0800

    Add files via upload

---
+++ tvkj.php
@@ -0,0 +1,12 @@
+{"urls":[{
+"url":"assets://tianwei.json","name":"💝app接口💝天微vip线"},
+{"name":"💝在线💝饭太硬","url":"http://www.饭太硬.com/tv"},
+{"name":"💝在线💝道长降魔","url":"https://pastebin.com/raw/5NHaxyGR"},
+{"name":"💝在线💝肥猫","url":"http://肥猫.com"},
+{"name":"💝在线💝喵影视","url":"http://meowtv.top/tv"},
+{"name":"💝在线💝PG线路","url":"https://git.acwing.com/iduoduo/orange/-/raw/main/jsm.json"},
+{"name":"💝在线💝潇洒线路","url":"https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/api.json"},
+{"name":"💝在线💝D佬线路","url":"https://download.kstore.space/download/2883/nzk/nzk0722.json"},
+{"name":"💝在线💝香雅情深","url":"https://github.moeyy.xyz/https://raw.githubusercontent.com/xyq254245/xyqonlinerule/main/XYQTVBox.json"}
+
+]}
